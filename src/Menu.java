58c5e5b6c29b411267d6ffc453038f3369041568
A-Annan <annanabdelilah@gmail.com>
2019-12-08 16:12:07 +0100

    add board as background img the root pane

--- src/Menu.java
+++ src/Menu.java
@@ -1,23 +1,33 @@
 import javafx.application.Application;
 import javafx.scene.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.scene.shape.Rectangle;
+
 public class Menu extends Application {
-	BorderPane root;
+	Pane root;
 	Scene scene;
-	Rectangle bill;
+
+	Background background = new Background(new BackgroundImage(new Image("table.jpeg"),BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT,BackgroundPosition.CENTER,BackgroundSize.DEFAULT));
+
+		Circle circle1=  new Circle(300,100,20);
+
 	public  void start(Stage window)throws Exception {
-		root = new BorderPane();
+		root = new Pane();
+		root.setBackground(background);
 		scene = new Scene(root);
-		bill = new Rectangle(600,300);
-		bill.setId("billboard");
-		root.setCenter(bill);
+		root.getChildren().add(circle1);
 		scene.getStylesheets().add("style.css");
+		window.setWidth(500);
+		window.setHeight(500);
 		window.setScene(scene);
 		window.show();
 	}
 	public static void main(String args []) {
 		  Application.launch(args);
 	  }
+
 }

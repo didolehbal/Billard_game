0fc1a7933a6f909f4a7cb731cc2aac99b9ef187f
didilehbal <habilmehdi96@gmail.com>
2020-01-07 00:12:57 +0100

    fix acceleration + refactor Ball

--- src/Ball.java
+++ src/Ball.java
@@ -1,4 +1,3 @@
-import javafx.scene.Group;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
@@ -8,22 +7,11 @@ import javafx.scene.transform.Rotate;
 
 import static javafx.scene.paint.Color.WHITE;
 
-/**
- * Created by Sadiqur Rahman on 5/19/2016.
- */
 public class Ball {
     public  Sphere sphere =new Sphere(10,40);
-
-    private double angle;
-    private double a=10;
     PVector velocity = new PVector(0,0);
     double power = 0;
-    double mass =1;
     int ballid;
-
-    Ball(){
-
-    }
 
     Ball( double x, double y, String imageName, Color color) {
         sphere.setLayoutX(x);
@@ -57,42 +45,14 @@ public class Ball {
         sphere.setLayoutY(y);
     }
     public void move(){
-
-       /* if(power < 2){
-            velocity.x = 0;
-            velocity.y = 0;
-            power = 0;
-        }
-        else {
-            double mag = velocity.magnitude();
-            System.out.print(mag+"  ");
-            if(mag <0.7 || mag > 1.3){
-                System.out.println("BEFOR: " + this );
-                mag = velocity.normalize();
-                power += mag;
-                System.out.println( this);
-
-            }
-            /*double mag =  velocity.normalize();
-            System.out.println(this + " mag="+mag);
-            power += mag;*/
-            /*double acc = power *  0.01;
-            if(acc < 1) acc =1;*/
-            /*double acc= power * 0.01;
-            if(acc < 1) acc = 1;
-            if(velocity.magnitude() < 1 )
-                velocity.normalize();
-            power -= mag ;
-            setX(getX() + velocity.x );
-            setY(getY() + velocity.y );
-            spin();
-        }
-        */
         double mag = velocity.magnitude();
             if(power > 1 && power > mag ){
-                power -= mag ;
-                setX(getX() + velocity.x );
-                setY(getY() + velocity.y );
+                if(mag < 1 ) mag = velocity.normalize();
+                double acc = 0.01 * power;
+                if(acc < 1 ) acc = 1;
+                power -= mag *acc;
+                setX(getX() + velocity.x*acc );
+                setY(getY() + velocity.y*acc );
                 spin();
             }
             else{
@@ -102,7 +62,6 @@ public class Ball {
             }
 
     }
-
     public double getX(){
         return  sphere.getLayoutX();
     }
@@ -117,31 +76,6 @@ public class Ball {
                 "Position= ("+ getX()+","+getY() +")"+
                 "power= "+power+
                 '}';
-    }
-
-    public void setAngle(double angle) {
-        this.angle = angle;
-    }
-
-
-
-
-
-    public void setA(double a) {
-        this.a = a;
-    }
-
-    public double getA() {
-        return a;
-    }
-
-
-    public void setBallid(int ballid) {
-        this.ballid = ballid;
-    }
-
-    public int getBallid() {
-        return ballid;
     }
     private void spin() {
             Rotate rx = new Rotate(0, 0, 0, 0, Rotate.X_AXIS);

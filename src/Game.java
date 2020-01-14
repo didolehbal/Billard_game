50f9cbb68c17c19c07e5362d66609dc776dc4d50
didilehbal <habilmehdi96@gmail.com>
2020-01-14 20:36:46 +0100

    game refactor

--- src/Game.java
+++ src/Game.java
@@ -2,10 +2,9 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -18,21 +17,29 @@ import java.util.concurrent.atomic.AtomicLong;
 
 
 public class Game {
-    BorderPane root;
-    Pane gameContainer;
-    ImageView imgV;
     BallsManager ballManager;
+    PlayerManager playerManager;
     ControlStick stick;
+
+
+
     static final double TABLE_WIDTH = 852 , TABLE_HEIGHT = 485;  //according to wiki  2.84 meters by 1.42 meters ratio : 2.84:1.42 => 852 x 426
     static final double HEAD_WIDTH=852,HEAD_HEIGHT=75;
     static final double WINDOW_WIDTH = Math.max(TABLE_WIDTH , HEAD_WIDTH), WINDOW_HEIGHT = TABLE_HEIGHT + HEAD_HEIGHT;
-    PVector mousePosition  = new PVector(0,0);
-    boolean isGameOver = true;
     static final long MAX_POWER = 2000;
+
+    BorderPane root;
+    Pane gameContainer;
+    ImageView imgV;
+    Button goback;
+
+    static boolean isGameOver = false;
+    static Player Winner = null;
+
     boolean isMousePressed = false;
     AtomicLong MousePressedTime = new AtomicLong();
-    PlayerManager playerManager;
     Circle holes[] = new Circle[6];
+    PVector mousePosition  = new PVector(0,0);
 
     public Game(){
         root = new BorderPane();
@@ -42,23 +49,17 @@ public class Game {
         playerManager = new PlayerManager();
     }
 
-    public void updateMousePosition() {
 
+
+    public void manageEvents(){
         gameContainer.setOnMouseMoved(e -> {
             mousePosition = new PVector(e.getX(),e.getY());
             stick.RotateStick(e.getX(),e.getY());
         });
-
-    }
-    public void manageKeysPress(){
-        gameContainer.setOnKeyPressed(e ->{
-            System.out.println("pressed : "+ e.getCharacter());
+        gameContainer.setOnScroll(e-> {
+                    isGameOver=true;
+                    Winner=PlayerManager.getPlayerWithTurn();
         });
-    }
-    public void manageMouseclick(){
-        gameContainer.addEventFilter(KeyEvent.KEY_PRESSED,
-                event -> System.out.println("Pressed: " + event.getCode()));
-
         gameContainer.setOnMouseClicked(e->{
             System.out.println(mousePosition);
             if(e.getButton() == MouseButton.SECONDARY){ // cancel shooting
@@ -98,33 +99,56 @@ public class Game {
         VBox player1 = new VBox();
         player1.getStyleClass().add("player-container");
         player1.getChildren().add(name1);
-         HBox player1Balls = PlayerManager.players.get(0).playerBallsView;
+        HBox player1Balls = new HBox();
         player1Balls.getStyleClass().add("balls-container");
         for(Ball b : playerManager.players.get(0).balls){
             player1Balls.getChildren().add(b.sphere);
         }
+        PlayerManager.players.get(0).playerBallsView= player1Balls;
         player1.getChildren().add(player1Balls);
 
         VBox player2 = new VBox();
         player2.getChildren().add(name2);
         player2.getStyleClass().add("player-container");
-        HBox player2Balls = PlayerManager.players.get(1).playerBallsView;
+        HBox player2Balls = new HBox();
         player2Balls.getStyleClass().add("balls-container");
         for(Ball b : playerManager.players.get(1).balls){
             player2Balls.getChildren().add(b.sphere);
         }
+        PlayerManager.players.get(1).playerBallsView = player2Balls;
         player2.getChildren().add(player2Balls);
 
         head.getChildren().addAll(player1,player2);
         head.setMinSize(HEAD_WIDTH,HEAD_HEIGHT);
         return head;
     }
+    public Pane getWinContainer(){
+        if(Winner != null ){
+            VBox vb = new VBox();
+            vb.getStyleClass().add("box-win");
+
+            Label lbl = new Label(Winner.name + " Has Won !");
+            lbl.getStyleClass().add("lbl-win");
+
+             goback = new Button("Go to Menu");
+            goback.getStyleClass().add("btn");
+            goback.setOnMouseClicked(e->{
+                System.out.println("click");
+                Main.activate("Menu");
+            });
+            vb.getChildren().addAll(lbl,goback);
+            return vb;
+        }
+        return null;
+    }
     public Pane getGameContainer(){
         imgV = new ImageView("assets/images/game-3.png");
 
+        isGameOver=false;
+        Winner=null;
+
         imgV.setFitWidth(TABLE_WIDTH);
         imgV.setFitHeight(TABLE_HEIGHT);
-        //imgV.setPreserveRatio(true);
         imgV.setSmooth(true);
         imgV.setCache(true);
         gameContainer.getChildren().add(imgV);
@@ -134,9 +158,7 @@ public class Game {
         stick.white = white;
         stick.drawStick();
         startTheGame();
-        manageMouseclick();
-        manageKeysPress();
-        updateMousePosition();
+        manageEvents();
         root.setCenter(gameContainer);
 
         root.setTop(getHeader());
@@ -163,7 +185,12 @@ public class Game {
                             ballManager.handlePlayerTurnUI();
                             if(stick.translationValue == 0)
                                 ballManager.updateBallsPositions();
-
+                        }
+                        else {
+                            gameLoop.stop();
+                            stick.hide();
+                            root.setTop(null);
+                            root.setCenter(getWinContainer());
                         }
                         // else show msg end
                     }
@@ -171,6 +198,5 @@ public class Game {
 
         gameLoop.getKeyFrames().add( kf );
         gameLoop.play();
-        isGameOver = false;
     }
 }
